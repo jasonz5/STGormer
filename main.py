0cb5a28363e4346e8e1c0d482a0f3ba5637cfd14
jasonz5 <jasonzhou314@gmail.com>
2024-04-15 20:37:51 +0000

    delete runme.sh

--- main.py
+++ main.py
@@ -77,6 +77,7 @@ def model_supervisor(args):
     return results
 
 if __name__=='__main__':
+    # python main_moe.py --gpu_id=$1 --config_filename=configs_moe/$2.yaml
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu_id', type=str, default='7', help='GPU ID to use')
     parser.add_argument('--config_filename', default='configs/moest/NYCBike1.yaml', 
--- runme.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# set the experiment GPU by the first parameters
-# export CUDA_VISIBLE_DEVICES=$1
-# set the dataset by the second parameters
-python main_moe.py --gpu_id=$1 --config_filename=configs_moe/$2.yaml
